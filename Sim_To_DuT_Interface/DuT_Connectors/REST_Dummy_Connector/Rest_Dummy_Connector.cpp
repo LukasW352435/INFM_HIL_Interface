d4479c83bbae90e4d0d363af6e4073d8f1103778
Michael Schmitz <michaelschmitz@live.de>
2021-11-02 15:47:07 +0100

    Ci (#5)
    * add cmake and docker build actions
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * make install dep only a placeholder
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * checkout submodules as well
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * install restbed deps
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * add libcurl dep
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * update to libcurl4
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * rewording for testing action
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * install curl headers as well
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * rewording to test action
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * explicitly use package (not the virtual one)
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * fix push path
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * rewording to test action
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * fix image tag
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * rewording to test action
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * fix docker action (typo in path)
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * fix docker action (submodule checkout)
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * add artifact upload of image
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * rewording to test action
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * try upload of docker image without image tag
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * extract commit sha in separate step
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * make curl a required package for REST_Dummy_DuT
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * remove unnecessary option, try fixing linker problem
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * change linking for libcurl to avoid github action failure
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * save built binaries as artifacts
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>
    
    * fix typo in Dockerfile
    
    Signed-off-by: Michael Schmitz <michaelschmitz@live.de>

--- Sim_To_DuT_Interface/DuT_Connectors/REST_Dummy_Connector/Rest_Dummy_Connector.cpp
+++ Sim_To_DuT_Interface/DuT_Connectors/REST_Dummy_Connector/Rest_Dummy_Connector.cpp
@@ -1,5 +1,26 @@
-//
-// Created by Lukas on 19.10.2021.
-//
+/**
+ * HIL - REST Dummy Connector
+ * This the connector to interface with the REST Dummy DuT
+ *
+ * Copyright (C) 2021 Michael Schmitz
+ *
+ * This file is part of "HIL - REST Dummy Connector".
+ *
+ * "HIL - REST Dummy DuT" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * "HIL - REST Dummy Connector" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "HIL - REST Dummy Connector".  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Michael Schmitz
+ * @version 1.0
+ */
 
 #include "Rest_Dummy_Connector.h"
