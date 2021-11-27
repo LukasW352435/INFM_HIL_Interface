d9b19604a0ff10458373f91b4fefbb5d7918a968
Michael Schmitz <michaelschmitz@live.de>
2021-11-27 17:01:31 +0100

    move comments into header, make cpp file private

--- Sim_To_DuT_Interface/DuTLogger/EventVisitor.cpp
+++ Sim_To_DuT_Interface/DuTLogger/EventVisitor.cpp
@@ -6,22 +6,6 @@
 
 #include "EventVisitor.h"
 
-/**
- * This function scans the string for special chars that can cause troubles in a CSV file.
- * After one of this character is found, it will be handled by the following rules:
- * <br>
- * <br>
- * 1. If the string contains a comma or double-quotes the string has to be encapsulated by two double-quotes <br>
- * 2. Each double-quote char will be replaced by a pair of double-quote
- *
- * @see
- * https://en.wikipedia.org/wiki/Comma-separated_values
- * @see
- * https://stackoverflow.com/questions/4617935/is-there-a-way-to-include-commas-in-csv-columns-without-breaking-the-formatting
- *
- * @param str   - Search for special characters in this string
- * @return a valid string which
-*/
 std::string EventVisitor::checkForSpecialChars(const std::string &str) {
     // search for special chars
     if (str.find(',') != std::string::npos || str.find('"') != std::string::npos) {
@@ -44,14 +28,6 @@ std::string EventVisitor::checkForSpecialChars(const std::string &str) {
     return str;
 }
 
-/**
- * This function replaces all occurrence of a specified string with another string.
- * It won't do anything if one of the parameters is empty.
- *
- * @param str       - this string will be modified by this function
- * @param fromThis  - search for this string and replace it
- * @param toThis    - replace it with this string
-*/
 void EventVisitor::replaceAll(std::string &str, const std::string &fromThis, const std::string &toThis) {
     // check for invalid arguments
     if (fromThis.empty() || toThis.empty()) {
--- Sim_To_DuT_Interface/DuTLogger/EventVisitor.h
+++ Sim_To_DuT_Interface/DuTLogger/EventVisitor.h
@@ -30,8 +30,29 @@ public:
         return checkForSpecialChars(str);
     }
 
-    // functions to convert strings into a valid CSV format
+    /**
+     * This function scans the string for special chars that can cause troubles in a CSV file.
+     * After one of this character is found, it will be handled by the following rules:
+     *
+     * 1. If the string contains a comma or double-quotes the string has to be encapsulated by two double-quotes
+     * 2. Each double-quote char will be replaced by a pair of double-quote
+     *
+     * @see https://en.wikipedia.org/wiki/Comma-separated_values
+     * @see https://stackoverflow.com/questions/4617935/is-there-a-way-to-include-commas-in-csv-columns-without-breaking-the-formatting
+     *
+     * @param str   - Search for special characters in this string
+     * @return a valid string which
+     */
     static std::string checkForSpecialChars(const std::string &str);
+
+    /**
+     * This function replaces all occurrence of a specified string with another string.
+     * It won't do anything if one of the parameters is empty.
+     *
+     * @param str       - this string will be modified by this function
+     * @param fromThis  - search for this string and replace it
+     * @param toThis    - replace it with this string
+     */
     static void replaceAll(std::string &str, const std::string &fromThis, const std::string &toThis);
 };
 
