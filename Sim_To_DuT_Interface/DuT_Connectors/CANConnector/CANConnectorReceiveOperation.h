76c9c6a96124c90e4576d4d5b417aadea140e1a2
mab6428 <mab6428@thi.de>
2021-11-27 17:42:02 +0000

    Added CANConnector Config Classes
    - Added CANConnector Config Class files without implementation

---
+++ Sim_To_DuT_Interface/DuT_Connectors/CANConnector/CANConnectorReceiveOperation.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ \project   INFM_HIL_Interface
+ \file      CANConnectorReceiveOperation.h
+ \brief     Contains the necessary information to configure a receive operation.
+            Used for the configuration of the CAN Connector.
+ \author    Matthias Bank
+ \version   1.0.0
+ \date      27.11.2021
+ ******************************************************************************/
+#ifndef SIM_TO_DUT_INTERFACE_CANCONNECTORRECEIVEOPERATION_H
+#define SIM_TO_DUT_INTERFACE_CANCONNECTORRECEIVEOPERATION_H
+
+
+/*******************************************************************************
+ * CLASS DECLARATIONS
+ ******************************************************************************/
+namespace sim_interface::dut_connector::can{
+
+    class CANConnectorReceiveOperation{
+
+    };
+
+}
+
+
+#endif //SIM_TO_DUT_INTERFACE_CANCONNECTORRECEIVEOPERATION_H
+/*******************************************************************************
+ * END OF FILE
+ ******************************************************************************/
---
+++ Sim_To_DuT_Interface/DuT_Connectors/CANConnector/CANConnectorSendOperation.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ \project   INFM_HIL_Interface
+ \file      CANConnectorSendOperation.h
+ \brief     Contains the necessary information to configure a send operation.
+            Used for the configuration of the CAN Connector.
+ \author    Matthias Bank
+ \version   1.0.0
+ \date      27.11.2021
+ ******************************************************************************/
+#ifndef SIM_TO_DUT_INTERFACE_CANCONNECTORSENDOPERATION_H
+#define SIM_TO_DUT_INTERFACE_CANCONNECTORSENDOPERATION_H
+
+
+/*******************************************************************************
+ * CLASS DECLARATIONS
+ ******************************************************************************/
+namespace sim_interface::dut_connector::can{
+
+    class CANConnectorSendOperation{
+
+    };
+
+}
+
+
+#endif //SIM_TO_DUT_INTERFACE_CANCONNECTORSENDOPERATION_H
+/*******************************************************************************
+ * END OF FILE
+ ******************************************************************************/
