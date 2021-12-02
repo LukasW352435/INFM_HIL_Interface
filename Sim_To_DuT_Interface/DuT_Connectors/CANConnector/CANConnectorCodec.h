596547bb97d03b9d63cc53c4d88bc75f8c441b9d
mab6428 <mab6428@thi.de>
2021-12-02 15:46:49 +0000

    Added CAN Connector Codec Class file
    - Added CAN Connector Codec Class file without implementation

---
+++ Sim_To_DuT_Interface/DuT_Connectors/CANConnector/CANConnectorCodec.h
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ \project   INFM_HIL_Interface
+ \file      CANConnectorCodec.h
+ \brief     The CAN Connector Codec defines the interface that each codec
+            implementation must fulfill.
+ \author    Matthias Bank
+ \version   1.0.0
+ \date      16.12.2021
+ ******************************************************************************/
+#ifndef SIM_TO_DUT_INTERFACE_CANCONNECTORCODEC_H
+#define SIM_TO_DUT_INTERFACE_CANCONNECTORCODEC_H
+
+
+/*******************************************************************************
+ * INCLUDES
+ ******************************************************************************/
+// Project includes
+#include "../../Events/SimEvent.h"
+
+// System includes
+
+
+/*******************************************************************************
+ * CLASS DECLARATIONS
+ ******************************************************************************/
+namespace sim_interface::dut_connector::can{
+
+    class CANConnectorCodec{
+
+    };
+
+}
+
+
+#endif //SIM_TO_DUT_INTERFACE_CANCONNECTORCODEC_H
+/*******************************************************************************
+ * END OF FILE
+ ******************************************************************************/
