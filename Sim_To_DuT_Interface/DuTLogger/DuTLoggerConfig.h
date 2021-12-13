e117e6069a835ea914b549fc5797dfe59eb95801
mak5338 <mak2712@thi.de>
2021-12-13 19:18:25 +0100

    Added new doc headers for files

--- Sim_To_DuT_Interface/DuTLogger/DuTLoggerConfig.h
+++ Sim_To_DuT_Interface/DuTLogger/DuTLoggerConfig.h
@@ -1,6 +1,22 @@
 /**
- * The DuTLoggerConfig contains essential values to configure the DuTLogger.
- * Every value will be explained in the documentation of this file.
+ * Sim To DuT Interface
+ *
+ * Copyright (C) 2021 Marco Keul
+ *
+ * This file is part of "Sim To DuT Interface".
+ *
+ * "Sim To DuT Interface" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * "Sim To DuT Interface" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Sim To DuT Interface".  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author Marco Keul
  * @author Lukas Wagenlehner
